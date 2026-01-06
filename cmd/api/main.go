7ded640f77f537cf55bff91c292b23c5cea24184
dcashman <dan.a.cashman@gmail.com>
2026-01-05 20:56:29 -0800

    Enable simple CORS rules, specified w/cmdline to enable testing of local frontend.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -41,6 +42,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -75,6 +79,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "292328e499a277", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "9f8310c421947f", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "EatInn <no-reply@eatinn.dcashman.net>", "SMTP sender")
+
+	// CORS settings
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
